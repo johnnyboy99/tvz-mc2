06ed41673b817f3f209fc2cb2fb1a8f99fd4d746
Assassin55 <borna.kc@gmail.com>
2015-04-21 20:18:24 +0200

    Dodan navigation drawer

--- CodeIt/app/src/main/java/tvz/mc2/codeit/MenuProba.java
+++ CodeIt/app/src/main/java/tvz/mc2/codeit/MenuProba.java
@@ -1,30 +1,56 @@
 package tvz.mc2.codeit;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
-public class MenuProba extends Activity {
+public class MenuProba extends Activity implements AdapterView.OnItemClickListener {
 
-    HorizontalScrollView hsv;
-    ImageView arrowl, arrowr;
     int maxScrollX;
+    private String [] izborArray;
+    @InjectView(R.id.horizontalScrollView) HorizontalScrollView hsv;
+    @InjectView(R.id.arrowl) ImageView arrowl;
+    @InjectView(R.id.arrowr) ImageView arrowr;
+    @InjectView(R.id.drawerGumb) ImageButton drawerGumb;
+    @InjectView(R.id.mainContent) FrameLayout frame;
+    @InjectView(R.id.navList) ListView mDrawerList;
+    @InjectView(R.id.drawer_layout) DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_proba);
-        hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
-        arrowl = (ImageView) findViewById(R.id.arrowl);
-        arrowr = (ImageView) findViewById(R.id.arrowr);
+        ButterKnife.inject(this);
+        izborArray = getResources().getStringArray(R.array.izbor);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.unsimple_list_item, izborArray));
+        mDrawerList.setOnItemClickListener(this);
+    }
+
+    @OnClick(R.id.drawerGumb)
+    public void onClick (View v)
+    {
+        drawerLayout.openDrawer(mDrawerList);
     }
 
 
@@ -90,5 +116,42 @@ public class MenuProba extends Activity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position)
+        {
+            case 0:
+                Toast.makeText(this, "Ovdje bude popup za tekst zadatka", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                restart();
+                break;
+            case 2:
+                izlaz();
+                break;
+            default:
+                break;
+        }
+        selectItem(position);
+    }
+
+    public void selectItem(int position) {
+        mDrawerList.setItemChecked(position, true);
+    }
+
+    public void restart()
+    {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+
+    public void izlaz()
+    {
+        Intent intent = new Intent(MenuProba.this, GlavniIzbornik.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
